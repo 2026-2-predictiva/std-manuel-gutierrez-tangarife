91cf085ff073818b3a7639971403065a44007db1
- Manuel Gutierrez <mgutierrezt98@gmail.com>
2026-09-19 09:16:12 -0500

    codigo_regresion_2_sin_error_mapeo

--- PRE_02_regresion_basica/tests/test_src.py
+++ PRE_02_regresion_basica/tests/test_src.py
@@ -11,9 +11,9 @@ def test_01():
 
     dataset = pd.read_csv(f"{FOLDER}/data/auto_mpg.csv")
     dataset = dataset.dropna()
-    dataset["Origin"] = dataset["Origin"].map(
-        {1: "USA", 2: "Europe", 3: "Japan"},
-    )
+    #dataset["Origin"] = dataset["Origin"].map(
+    #    {1: "USA", 2: "Europe", 3: "Japan"},
+    #)
     dataset = pd.get_dummies(dataset, columns=["Origin"], prefix="", prefix_sep="")
     y_true = dataset.pop("MPG")
 
